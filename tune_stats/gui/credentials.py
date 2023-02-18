bb2a28a559e37b7b79b2c90620129a3801b11d84
Ole Gligorov <72470450+olegligorov@users.noreply.github.com>
2023-02-19 01:07:14 +0200

    Update credentials.py

--- tune_stats/gui/credentials.py
+++ tune_stats/gui/credentials.py
@@ -1,4 +1,4 @@
-CLIENT_ID = '2c748ba73cfa404d92b3d73e615374fe'
-CLIENT_SECRET = '5d8d716c4ee049ff9f1cd65c354432c6'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
 REDIRECT_URI = 'http://127.0.0.1:8000/tunestats/redirect'
 AUTHORIZATION_BASE_URL = "https://accounts.spotify.com/authorize"
